f69e8ea08bb5e3b259c3d75a8b3da6a4ef8abd14
opentrons_user <opentrons_user>
2017-04-05 10:59:53 +0200

    opentrons via notebook

--- dilution_p200_simple.py
+++ dilution_p200_simple.py
@@ -3,8 +3,8 @@ from opentrons import containers, instruments
 
 p200rack = containers.load('tiprack-200ul', 'A1', 'tiprack')
 plate = containers.load('96-PCR-flat', 'C1', 'plate')
-trough = containers.load('trough-12row', 'C3', 'trough')
-trash = containers.load('point', 'A1', 'trash')
+trough = containers.load('tube-rack-.75ml', 'C3', 'trough')
+trash = containers.load('point', 'A3', 'trash')
 
 p200 = instruments.Pipette(
     name="p200",
@@ -14,42 +14,43 @@ p200 = instruments.Pipette(
     axis="b"
 )
 
-p200.pick_up_tip(p200rack['A1'])
+p200.pick_up_tip(p200rack['A2'])
+p200.aspirate(200, trough['A1'])  # first row in trough
+p200.dispense(100, plate['G6'])
+p200.dispense(50, plate['G7'])
+p200.dispense(25, plate['G8'])
+p200.drop_tip(p200rack['A2'])
 
-p200.aspirate(100, trough['A1'])  # first row in trough
-p200.dispense(100, plate['F1'])
+p200.pick_up_tip(p200rack['A3'])
+p200.aspirate(200, trough['B1'])  # first row in trough
+p200.dispense(100, plate['G6'])
+p200.dispense(100, plate['G7'])
+p200.aspirate(200, trough['B1'])  # first row in trough
+p200.dispense(175, plate['G8'])
+p200.aspirate(50, trough['B1'])  # first row in trough
+p200.dispense(50, plate['G7'])
+p200.drop_tip(p200rack['A3'])
 
-p200.aspirate(25, trough['A3'])  # first row in trough
-p200.dispense(25, plate['F1'])
-
-p200.aspirate(75, trough['A5'])  # first row in trough
-p200.dispense(75, plate['F1'])
-
-p200.aspirate(100, trough['A5'])  # first row in trough
-p200.dispense(100, plate['F2'])
-
-p200.aspirate(100, trough['A5'])  # first row in trough
-p200.dispense(100, plate['F3'])
-
-for i in range(3):
-	p200.aspirate(100, plate['F1'])  # first row in trough
-	p200.dispense(100, plate['F1'])
-
-p200.aspirate(100, plate['F1'])  # first row in trough
-p200.dispense(100, plate['F2'])
-
-for i in range(3):
-	p200.aspirate(100, plate['F2'])  # first row in trough
-	p200.dispense(100, plate['F2'])
-
-p200.aspirate(100, plate['F2'])  # first row in trough
-p200.dispense(100, plate['F3'])
+p200.pick_up_tip(p200rack['A4'])
+p200.aspirate(150, trough['C1'])  # first row in trough
+p200.dispense(50, plate['G6'])
+p200.dispense(50, plate['G7'])
+p200.dispense(50, plate['G8'])
 
 for i in range(3):
-	p200.aspirate(100, plate['F3'])  # first row in trough
-	p200.dispense(100, plate['F3'])
+	p200.aspirate(100, plate['G8'])  # first row in trough
+	p200.dispense(100, plate['G8'])
 
-p200.drop_tip(p200rack['A1'])
+for i in range(3):
+	p200.aspirate(100, plate['G7'])  # first row in trough
+	p200.dispense(100, plate['G7'])
+
+for i in range(3):
+	p200.aspirate(100, plate['G6'])  # first row in trough
+	p200.dispense(100, plate['G6'])
+
+
+p200.drop_tip(p200rack['A4'])
 
 #well = ['A1', 'B1']
 #well = ['A1']
